4f5ffae09088d9a88056367fa76683ebe890237b
Konrad-Markowski <k.markowski2002@gmail.com>
2024-02-23 11:03:52 +0100

    zad1

---
+++ lab1/klocki_1.pl
@@ -0,0 +1,19 @@
+/*
+PROGRAM: klocki_1
+Jest to baza widzy o układzie klocków
+na(X,Y)
+opis: spełniony, gdy klocek X leży bezpośrednio na klocku Y
+*/
+
+na(c,a).
+na(c,b).
+na(d,c).
+
+/*
+Informacje o budowe programu:
+Program składa się z 3 klauzul.
+Program zawiera 1 definicję relacji.
+Jest to relacja na/2.
+Definicja relacji na/2 składa się z 
+
+*/
