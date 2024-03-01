ba8fc5cc8fd888c1c97c463495a4942f4d126c6f
Konrad-Markowski <k.markowski2002@gmail.com>
2024-03-01 10:10:36 +0100

    Update klocki_1.pl

--- lab1/klocki_1.pl
+++ lab1/klocki_1.pl
@@ -29,6 +29,9 @@ Klocek X leży bezpośrednio pod klockiem Y
 miedzy(X,Y,Z):-na(X,Y), pod(X,Z).
 miedzy(X,Y,Z):-na(X,Z), pod(X,Y).
 
+% alternatywny sposób: miedzy(X,Y,Z):-na(X,Y), na(Z,X).
+%                      miedzy(X,Y,Z):- na(X,Z), na(Y,X).
+
 /*
 Klocek X leży pomiędzy klockami Y i Z
 */
