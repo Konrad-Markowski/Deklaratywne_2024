b3c643cad17aab98f497b231091996ec1a7777a4
Konrad-Markowski <k.markowski2002@gmail.com>
2024-04-12 10:58:29 +0200

    Update listy2.pl

--- lab7/listy2.pl
+++ lab7/listy2.pl
@@ -15,3 +15,15 @@ wiekszy_od(X,Y):- Y>X.
 
 %wiekszy_od_lista(X,L) - spełniony, gdy każdy element listy L jest większy od X
 wiekszy_od_lista(X,L):- maplist(wiekszy_od(X),L).
+
+%wieksza_lista(L1,L2) - spełniony, gdy elementy L2 są większe od odpowiednich elementów L1
+wieksze(X,Y):- Y>X.
+wieksza_lista(L1,L2):-maplist(wieksze,L1,L2).
+
+%razy2_lista(L1,L2)-spełniony gdy elementy L2 są odpowiednio 2 razy większe niż elementy listy L1
+podwojone(X,Y):- Y is 2*X.
+razy2_lista(L1,L2):-maplist(podwojone,L1,L2).
+
+%kwadrat_lista(L1,L2)
+kwadrat(X,Y):- Y is X*X.
+kwadrat_lista(L1,L2):-maplist(kwadrat,L1,L2).
